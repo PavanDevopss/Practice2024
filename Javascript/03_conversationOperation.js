305368b84333ff7013b0c101bac55cba7c83f962
PavanDevopss <iampavan.blue@gmail.com>
2024-12-03 20:12:59 +0530

    Conversation of Datatypes

---
+++ Javascript/03_conversationOperation.js
@@ -0,0 +1,40 @@
+// let score = "33abc"
+
+// console.log(typeof score);
+
+// console.log(typeof (score));
+
+// let valueNumber = Number(score)
+
+// console.log(typeof valueNumber)
+// console.log(valueNumber)
+
+// let isLoggedIn = 1
+
+// let booleanIsloggedIn = Boolean(isLoggedIn)
+
+// console.log(booleanIsloggedIn);
+// console.log(typeof booleanIsloggedIn);
+
+// let isLoggedIn = ""
+
+// let booleanIsloggedIn = Boolean(isLoggedIn)
+
+// console.log(typeof booleanIsloggedIn);
+// console.log(booleanIsloggedIn);
+
+// let isLoggedIn = "123"
+
+// let booleanIsloggedIn = Boolean(isLoggedIn)
+
+// console.log(typeof booleanIsloggedIn);
+// console.log(booleanIsloggedIn);
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
+
