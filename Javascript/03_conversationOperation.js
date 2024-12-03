f389ceafcb12f59b5cd11815c1c369870e83c3ba
PavanDevopss <iampavan.blue@gmail.com>
2024-12-03 20:46:06 +0530

    added operations

--- Javascript/03_conversationOperation.js
+++ Javascript/03_conversationOperation.js
@@ -30,11 +30,64 @@
 // console.log(typeof booleanIsloggedIn);
 // console.log(booleanIsloggedIn);
 
-let someNumber = 33
+// let someNumber = 33
 
-let stringNumber = String(someNumber)
+// let stringNumber = String(someNumber)
 
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
 
 
+// ********************Operations*******************
+
+
+// let value = 5
+// let Negvalue = -value
+
+// console.log(Negvalue);
+
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2/2);
+// console.log(2%2);
+// console.log(2**3);
+
+// console.table([2+2,2-1,2*2,2**3,2%2]);
+
+
+// let str1 = "Hello"
+// let str2 =  " Pavan"
+// let str3 =  (str1+str2)
+
+// console.log(str3);
+
+
+// console.log("1"+2);
+// console.log(1+"2");
+// console.log("1"+"2");
+// console.log("1" + 2 + 5 );
+// console.log( 1 + 2 + "5" );
+
+
+// console.log( (1 + 2) * 5 + (35/5) + "5");
+
+// console.log(+true);
+// console.log(+"true");
+// // console.log("true"++);
+// console.log(+"false");
+// console.log(+false);
+
+
+let x = 3;
+const y = x++;
+
+console.log(`x:${x}, y:${y}`);
+// Expected output: "x:4, y:3"
+
+let a = 3;
+const b = ++a;
+
+console.log(`a:${a}, b:${b}`);
+// Expected output: "a:4, b:4"
