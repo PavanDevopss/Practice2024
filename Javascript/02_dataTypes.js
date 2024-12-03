cffebd4b559b025c4d2908442e7c4c06cbeed7bc
Pavan <102276073+PavanDevopss@users.noreply.github.com>
2024-12-03 18:53:04 +0530

    Modified Files And Folders

---
+++ Javascript/02_dataTypes.js
@@ -0,0 +1,21 @@
+"use strict";
+
+// alert( 3 + 3 ) // we are using nodejs not browser
+
+
+let name = "pavan"
+let age = 20
+let isLoggedIn = false
+
+// number => 2 to the power of 53
+
+// number, bigint, string, boolean = true ot false, 
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+
+// object 
+
+console.log(typeof undefined);
+console.log(typeof null);
---
+++ Javascript/test.js
@@ -0,0 +1,4 @@
+console.log("Pavan");
+
+console.log("Chinnu");
+
