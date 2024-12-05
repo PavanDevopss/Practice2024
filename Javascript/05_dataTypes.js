61d08dafc960bd5ebdf313da8142779ee0804e68
PavanDevopss <iampavan.blue@gmail.com>
2024-12-05 16:46:46 +0530

    datatypes

---
+++ Javascript/05_dataTypes.js
@@ -0,0 +1,50 @@
+// primative data types
+
+// There are 7 data types String, number, boolean, undefined, null, Symbol, BigInt. 
+
+const india = "vizag" 
+console.log(typeof india);
+
+const score = 100
+console.log(typeof score);
+
+const scoreValue = 100.3
+console.log(typeof scoreValue);
+
+const isLoggedIn = false
+console.log(typeof isLoggedIn);
+
+const myNum = 2836483929729174n
+console.log(typeof myNum);
+
+const outsideTemp = null
+console.log(typeof outsideTemp);
+
+const userEmail = undefined
+console.log(typeof userEmail);
+
+
+// non primitive datatypes in javascript
+
+// Array , object, function
+
+let myhero = ["Hanuman", "Shaktiman", "Naagraj"]
+console.log(typeof myhero);
+
+let myObj = {
+    name: "Pavan",
+    age: 26,
+}
+console.log(typeof myObj);
+
+let myFun = function(){
+    console.log("Hello_world");
+}
+console.log(typeof myFun);
+
+console.log("These are all the Datatypes in javaScript");
+
+
+
+
+
