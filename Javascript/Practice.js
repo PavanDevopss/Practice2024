8370b04066bae51b6bc3cb8e6af9c425a4a0a7d4
PavanDevopss <iampavan.blue@gmail.com>
2024-12-06 17:18:33 +0530

    practiced and updated the String datatypes

---
+++ Javascript/Practice.js
@@ -0,0 +1,22 @@
+const name = "pavanKumar-pava-palu-devotee"
+const appCount = 33
+const myValley = 1000
+
+let botli = (name.slice(0,10));
+
+let myOutputting = (`There is a person named ${botli} , he has developed ${appCount} number of Apps for various usages, for each App there are like ${myValley} disscussions taken for his talent`);
+
+console.log(myOutputting);
+
+console.log(name.toUpperCase());
+console.log(name.slice(0,4));
+console.log(name.replace("-"," alias "));
+console.log(myOutputting.replace("-","alias"));
+console.log(myOutputting.trim());
+console.log(myOutputting.toUpperCase());
+console.log(myOutputting.toLowerCase());
+console.log(myOutputting.length);
+console.log(myOutputting.__proto__);
+console.log(myOutputting.charAt(9));
+console.log(myOutputting.indexOf("d"));
+console.log(myOutputting.split(" "));
