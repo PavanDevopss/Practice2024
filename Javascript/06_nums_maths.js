f819c535cafade18e0cdcb728618c0d42566c5b9
PavanDevopss <iampavan.blue@gmail.com>
2024-12-17 17:29:15 +0530

    Added Maths file

---
+++ Javascript/06_nums_maths.js
@@ -0,0 +1,88 @@
+// const score = 200
+// console.log(score);
+
+// const balance = new Number (986)
+// console.log(balance);
+// console.log(typeof balance);
+
+
+// console.log(balance.toString());
+// console.log(balance.toFixed(2));
+// console.log(balance.toString().length);
+// console.log(balance.toPrecision());
+
+// const otherNumber = 9980.0099688
+// console.log(otherNumber.toPrecision(4));
+
+// const anotherNumber = 5496.88649
+// console.log(anotherNumber.toPrecision(6));
+
+// const hundred = 1000000
+// console.log(hundred.toLocaleString('en-IN'));
+
+//********************Maths************************/
+
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.ceil(4.1));
+// console.log(Math.round(4.4));
+// console.log(Math.floor(4.9));
+// console.log(Math.min(4.7));
+// console.log(Math.max(4.2));
+
+// console.log(Math.random());
+// console.log(Math.random()*10 + 1 );
+// console.log(Math.floor(Math.random()*10) + 1 );
+
+// const min = new Number(10)
+// const max = new Number(20)
+
+// console.log(min);
+// console.log(typeof min);
+// console.log(max);
+// console.log(typeof max);
+
+const min = 10
+const max = 20
+
+// console.log(min);
+// console.log(typeof min);
+// console.log(max);
+// console.log(typeof max);
+
+console.log(Math.ceil(Math.random() * ((max - min) + 1)));
+
+// const score = 209
+// console.log(score);
+
+// const balance = new Number (1093.97)
+// console.log(balance);
+
+// const anotherNumber = 2394.02823
+// console.log(anotherNumber);
+
+// console.log(anotherNumber.toString())
+// console.log(anotherNumber.toString().length)
+// console.log(anotherNumber.toFixed(5))
+// console.log(anotherNumber.toPrecision(6))
+// console.log(anotherNumber.toLocaleString('en-IN'))
+
+
+// ***************Math********************
+
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.floor(4.8));
+// console.log(Math.ceil(4.1));
+// console.log(Math.round(4.49));
+// console.log(Math.min(4.7));
+// console.log(Math.max(4.2));
+
+// console.log(Math.random());
+// console.log(Math.random()*10);
+// console.log(Math.random()*10+1);
+
+const Amin = 10
+const Amax = 20
+
+console.log(Math.floor(Math.random() * (Amax - Amin) + 1));
