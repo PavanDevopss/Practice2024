ad25c10ba5d41392bb4adc30817abfa9978fdcde
PavanDevopss <iampavan.blue@gmail.com>
2025-01-23 15:07:34 +0530

    added git ignore and practiced functions

--- Javascript/10_functions.js
+++ Javascript/10_functions.js
@@ -1,4 +1,101 @@
 // console.log("P");
+// console.log("a");
+// console.log("v");
+// console.log("a");
+// console.log("n");
+
+// function sayMyName (){
+//     console.log("P");
+//     console.log("a");
+//     console.log("v");
+//     console.log("a");
+//     console.log("n");
+// }
+
+// sayMyName()
+
+// function add2Numbers (num1 ,num2){
+//     console.log(num1 + num2);
+// }
+
+// add2Numbers(2,3)
+// add2Numbers(2,9)
+// add2Numbers(2,"2")
+// add2Numbers(2,"a")
+// function add2Numbers(num1,num2,num3){
+//     let result = (num1+num2+num3)
+//     return result
+// }
+
+// const result = add2Numbers(2,0,0)
+// console.log("Result: ", result);
+// function add9nums(num1,num2,num3,num4,num5,num6,num7,num8,num9){
+//     let result = (num1+num2+num3+num4+num5+num6+num7+num8+num9)
+//     return result
+// }
+
+// const result = add9nums(50,150,400,1000,2500,6000,14000,26000,55000)
+// console.log("total Invested Amount :", result);
+
+
+// function multi2num(num10,num11){
+//     let result1 = (num10*num11)
+//     return result1
+// }
+
+// const result1 = multi2num(55000,2)
+
+// console.log("Total Gained Amount", result1);
+
+// function sub2num (num12,num13){
+//     let result2 = (num12 - num13)
+//     return result2
+// }
+
+// const result2 = sub2num(result1,result)
+
+// console.log("Remained Profit", result2);
+
+// function loginUserMessage(username){
+//     console.log(`"${username} has just logged In"`);    
+// }
+
+// loginUserMessage("Pavan_K")
+
+// function loginUserMessage(username){
+//     if(username === undefined){
+//         console.log(`"username is ${username} , please check"`);    
+//         return
+//         }
+//         return `${username} Has Just Logged In`
+// }
+
+// // loginUserMessage()
+// console.log(loginUserMessage("Pavan"));
+
+// function bookMessage(msg1){
+//     if(!msg1){
+//         console.log(`Kindly please check your order ${msg1} hasn't booked yet try again`);
+//         return
+//     }
+//        return (`congratulations ${msg1} your order has been booked Sir`)
+// }
+
+// // bookMessage()
+// console.log(bookMessage());
+
+// function bookMessage(msg1){
+//     if(!msg1){
+//         console.log(`Kindly please check your order ${msg1} hasn't booked yet try again`);
+//         return
+//     }
+//        return (`congratulations ${msg1} your order has been booked Sir`)
+// }
+
+// // bookMessage()
+// console.log(bookMessage("'Fasak'"));
+
+// console.log("P");
 // console.log("A");
 // console.log("V");
 // console.log("A");
@@ -33,14 +130,14 @@
 // const result = addTwoNumbers(2,3)
 
 
-function addTwoNumbers(number1,number2){
-    // let result = (number1 + number2)
-    // return result 
-    return number1 + number2
-    // console.log("Pavan"); --> this will not print 
-}
+// function addTwoNumbers(number1,number2){
+//     // let result = (number1 + number2)
+//     // return result 
+//     return number1 + number2
+//     // console.log("Pavan"); --> this will not print 
+// }
 
-const result = addTwoNumbers(2,3)
+// const result = addTwoNumbers(2,3)
 
 // console.log( "Result: ", result );
 
@@ -68,12 +165,12 @@ const result = addTwoNumbers(2,3)
 
 // console.log(fbUsername());
 
-function fbUsername(username = "Thanu"){
-    if(!username){
-        console.log(" Username is not defined , Please enter : ");
-        return
-        }
-    return `Please enter your username:  ${username}`
-}
+// function fbUsername(username = "Thanu"){
+//     if(!username){
+//         console.log(" Username is not defined , Please enter : ");
+//         return
+//         }
+//     return `Please enter your username:  ${username}`
+// }
 
-console.log(fbUsername("Pavan"));
+// console.log(fbUsername("Pavan"));
