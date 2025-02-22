3ff65f9aab3e728431dbe8a361eecc7a791a736c
PavanDevopss <iampavan.blue@gmail.com>
2025-02-22 23:05:31 +0530

    Practiced loops 2nd time

--- Javascript/15_02_Loops.js
+++ Javascript/15_02_Loops.js
@@ -28,7 +28,7 @@
 //     score++
 // } while (score <= 10);
 
-// -----------------Practice -------------------------
+// -----------------Practice ------------------------- 1---------
 //  I've take for loop as example created this code
 
 
@@ -59,9 +59,46 @@
 //     arr = arr + 1
 // }
 
-let score = 11
+// let score = 11
+
+// do {
+//     console.log(`The value of the Score is : ${score}`);
+//     score++
+// } while (score <= 10);
+
+
+// -----------------Practice ------------------------- 2 ---------
+
+
+// for (let i = 1; i <= 15; i++) {
+//     console.log(`This is the Multiplication table of: ${i}`);
+//     for (let j = 1; j <= 15; j++) {
+//         console.log(`${i} * ${j} = ${i*j}`);
+//     }
+// }
+
+// let indu = 1
+// while (indu < 15) {
+//     console.log(`This is the Multiplication Table of ${indu}`);
+//     indu = indu + 1
+//     let binu = 1
+//     while (binu < 15) {
+//         console.log(`${indu} * ${binu} = ${indu*binu}`);
+//         binu++
+//     }
+// }
+
+// let myData = ["Dad","Mom","Child"]
+
+// let arr = 0
+// while (arr < myData.length) {
+//     console.log(`This the Value of the myData: ${myData[arr]}`);
+//     arr++
+// }
+
+let score = 1
 
 do {
-    console.log(`The value of the Score is : ${score}`);
+    console.log(`The Score of the Match is : ${score}`);
     score++
 } while (score <= 10);
