3dc88bd891964fafe6a03ce67e37a83574b16ba1
PavanDevopss <iampavan.blue@gmail.com>
2025-02-22 20:02:41 +0530

    Added the loops and updated thr code

---
+++ Javascript/15_01_Loops.js
@@ -0,0 +1,122 @@
+// for
+
+// for (let index = 1; index <= 10; index++) {
+//     const element = index;
+//     console.log(element);  
+// }
+
+// for (let i = 1; i <= 10; i++) {
+//     const element = i;
+//     console.log(element);  
+// }
+
+// console.log(element);  
+
+// for (let i = 1; i <= 10; i++) {
+//     const element = i;
+//     if (element == 5) {
+//         console.log(`This Number ${element}, is Best number`);
+        
+//     }
+//     console.log(element);  
+// }
+
+// for (let i = 1; i <= 15; i++) {
+//     console.log(`This is "${i}" Multiplication Table`);
+    
+//     for (let j = 1; j <= 15; j++) {
+//         // console.log(`Inner loop value ${j}, Inner loop for i : ${i}`);
+//             // console.log(i + '*' + j + '=' + i*j);
+//             console.log(`${i} * ${j} = ${i*j}` );
+            
+//     }
+    
+// }
+
+// let myArray = ["Hanuman","Kalabairav","Shivaya_Namaha"]
+
+// for (let index = 0; index < myArray.length; index++) {
+//     const element = myArray[index];
+//     console.log(element);
+// }
+
+// let myData = ["Pavan","Kanaka","Thanu","Jishna"]
+// console.log(myData.length);
+
+// for (let i = 0; i < myData.length; i++) {
+//     const element = myData[i];
+//     console.log(`The Value of element is : ${element}`);
+// }
+
+// break and continue
+
+// for (let i = 0; i <= 20; i++) {
+//     if (i ==5) {
+//         console.log(`Detected 5`);
+//         break
+//     }
+//     console.log(`Value of i is : ${i}`);
+// }
+
+// for (let i = 0; i <= 20; i++) {
+//     if (i ==5) {
+//         console.log(`Detected and Skiped 5`);
+//         continue
+//     }
+//     console.log(`Value of i is : ${i}`);
+// }
+
+// ----------------------------------------Practice ------------------------------
+
+// for (let index = 0; index <=10 ; index++) {
+//     const element = index;
+//     console.log(element);
+// }
+
+// for (let i = 0; i <=10; i++) {
+//     const element = i;
+//     console.log(element);
+// }
+
+// for (let i = 0; i <=10 ; i++) {
+//     const element = i;
+//     if (element == 9) {
+//         console.log(`We have detected the number: ${element}`);
+//     }
+//     console.log(element);
+// }
+
+// for (let i = 0; i <= 15; i++) {
+//     const element = i;
+//     console.log(`This is Multiplication Table of ${element}`);
+    
+//     for (let j = 1; j <=15 ; j++) {
+//         const element = j;
+//         console.log(`${i} * ${j} = ${i*j}`);
+//     }
+// }
+
+// let myHeros = ["Hanuman","Ramji","Laxman","ShivParvathiji"]
+// console.log(myHeros.length);
+
+// for (let i = 0; i < myHeros.length; i++) {
+//     const element = myHeros[i];
+//     console.log(element);
+// }
+
+
+// for (let i = 0; i <= 10; i++) {
+//     if ( i == 5) {
+//     console.log(`We have detected the value of ${i}`);
+//     break
+//     }
+//     console.log(i);
+// }
+
+// for (let i = 0; i <= 10; i++) {
+//     if ( i == 5) {
+//     console.log(`We have detected the value of ${i}`);
+//     continue
+//     }
+//     console.log(i);
+// }
---
+++ Javascript/15_02_Loops.js
@@ -0,0 +1,67 @@
+
+// let index = 0
+
+// while (index <= 10) {
+//     console.log(`Value of index is ${index}`);
+//     index = index + 1 
+// }
+
+// let myArray = ["Kalbairav","Vinayak","Shivaji","Parvathiji"]
+
+// let arr = 0
+// while (arr < myArray.length) {
+//     console.log(`Value is ${myArray[arr]}`);
+//     arr = arr + 1 
+// }
+
+// let score = 1
+
+// do {
+//     console.log(`score is ${score} `);
+//     score++
+// } while (score <= 10);
+
+// let score = 11
+
+// do {
+//     console.log(`score is ${score} `);
+//     score++
+// } while (score <= 10);
+
+// -----------------Practice -------------------------
+//  I've take for loop as example created this code
+
+
+// let index = 0
+
+// while (index <= 10) {
+//     console.log(`The Value of the index is : ${index}`);
+//     index = index + 1 
+// }
+
+
+
+
+// let myArray = ["Vinayaka","Kalbairavam","Shivaji","Parvathi_ji","Karthikeyan"]
+
+// let arr = 0
+// while ( arr < myArray.length ) {
+//     console.log(`The value of the MyArray is : ${myArray[arr]}`);
+//     arr++
+// }
+
+// let myData = ["Pavan","Chinnu","Thanu"]
+
+// let arr = 0
+
+// while (arr < myData.length) {
+//     console.log(`The value of the myData is : ${myData[arr]}`);
+//     arr = arr + 1
+// }
+
+let score = 11
+
+do {
+    console.log(`The value of the Score is : ${score}`);
+    score++
+} while (score <= 10);
